0c4043787c705912fdeece51e8936dbc89cec0bd
evrey666 <shwarzman.art@yandex.ru>
2022-09-30 20:36:12 +0300

    homework_2

--- js/script.js
+++ js/script.js
@@ -20,7 +20,7 @@
 // Проверить, чтобы все работало без ошибок в консоли 
 'use strict';
 
-const numberOfFilms = prompt('сколько фильмов вы уже посмотрели?', '');
+const numberOfFilms = +prompt('сколько фильмов вы уже посмотрели?', '');
 const personalMovieDB = {
   count: numberOfFilms,
   movies: {},
@@ -29,13 +29,28 @@ const personalMovieDB = {
   privat: false
 };
 
-const a = prompt('Один из последних просмотренных фильмов?', ''),
-      b = prompt('На сколько оцените его?', ''),
-      c = prompt('Один из последних просмотренных фильмов?', ''),
-      d = prompt('На сколько оцените его?', '');
+let i = 0;
+do  {
+  i++;
+  const a = prompt('Один из последних просмотренных фильмов?', ''),
+        b = prompt('На сколько оцените его?', '');
+  if (a != null && b != null && a != '' && b != '' && a.length < 50) {
+    personalMovieDB.movies[a] = b;
+    console.log('done');
+  } else {
+    console.log('error');
+  }
+} while (i <2);
 
-personalMovieDB.movies[a] = b;
-personalMovieDB.movies[c] = d;
+if (personalMovieDB.count < 10) {
+  console.log('Просмотрено довольно мало фильмов');
+} else if (personalMovieDB.count >= 10 && personalMovieDB.count < 30) {
+  console.log('Вы классический зритель');
+} else if (personalMovieDB.count >= 30) {
+  console.log('Вы киноман');
+} else {
+  console.log('Вы долбоёб))))))');
+}
 
 console.log(personalMovieDB);
 
@@ -55,5 +70,7 @@ console.log(personalMovieDB);
 
 
 
-
 // Код возьмите из предыдущего домашнего задания
+
+
+
