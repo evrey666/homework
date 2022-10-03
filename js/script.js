3fb14a445df0086cce6191ef8b54c089244b126a
evrey666 <shwarzman.art@yandex.ru>
2022-10-03 20:14:17 +0300

    добавлена проверка пробелов

--- js/script.js
+++ js/script.js
@@ -32,7 +32,7 @@ function rememberMyFilms() {
   let i = 0;
   do  {
     i++;
-    const a = prompt('Один из последних просмотренных фильмов?', ''),
+    const a = prompt('Один из последних просмотренных фильмов?', '').trim(),
           b = prompt('На сколько оцените его?', '');
     if (a != null && b != null && a != '' && b != '' && a.length < 50) {
       personalMovieDB.movies[a] = b;
